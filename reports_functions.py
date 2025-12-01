a912d9e019e4628832e9bc3a4f9b730192551070
Hedgewise Cosultoria <hedgewise.consultoria@gmail.com>
2025-12-01 15:55:37 -0300

    Update reports_functions.py

--- reports_functions.py
+++ reports_functions.py
@@ -654,3 +654,6 @@ def secao_relatorios_dashboard(df_transacoes, PLANO_DE_CONTAS):
         })
         st.dataframe(df_notas, hide_index=True)
         st.json(indicadores)
+
+st.write(df_transacoes.head(20))
+st.write(df_transacoes.dtypes)
