7187c2b41eadd0b318caf0313a806e2894d812e2
Hedgewise Cosultoria <hedgewise.consultoria@gmail.com>
2026-02-03 17:54:50 -0300

    Update auth.py

--- auth.py
+++ auth.py
@@ -21,10 +21,10 @@ SUPABASE_URL = st.secrets["SUPABASE_URL"]
 SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
 SUPABASE_SERVICE_ROLE_KEY = st.secrets["SUPABASE_SERVICE_ROLE_KEY"]
 
-supabase_admin = create_client(
-    SUPABASE_URL,
-    SUPABASE_SERVICE_ROLE_KEY
-)
+#supabase_admin = create_client(
+  #  SUPABASE_URL,
+    #SUPABASE_SERVICE_ROLE_KEY
+#)
 
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 LOGO_URL = "FinanceAI_1.png"
@@ -347,6 +347,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
 
